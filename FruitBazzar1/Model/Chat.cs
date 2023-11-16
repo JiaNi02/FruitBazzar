f6447bf7161e23c209afa66fcb55c4d3ae71d96a
Penny Lau <pennylau715@gmail.com>
2023-11-16 15:20:22 +0800

    details, cart

--- FruitBazzar1/Model/Chat.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FruitBazzar1.Model
-{
-    public class ChatMessage : ContentPage
-	{
-		public string Text { get; set; }
-		public bool IsIncoming { get; set; }
-	}
-}
--- FruitBazzar1/Views/ChatPage.xaml.cs
+++ FruitBazzar1/Views/ChatPage.xaml.cs
@@ -5,27 +5,11 @@ namespace FruitBazzar1.Views;
 
 public partial class ChatPage : ContentPage
 {
-	private ObservableCollection<ChatMessage> messages;
+	
 	public ChatPage()
 	{
 		InitializeComponent();
 
-		messages = new ObservableCollection<ChatMessage>();
-		chatListView.ItemsSource = messages;
-
-		//Initialize converters
-		Resources.Add("MessageAlignmentConverter", new MessageAlignmentConverter());
-		
 	}
 
-	private void OnSendMessageCompleted(object sender, EventArgs e)
-	{
-		string userMessage = userInputEntry.Text;
-
-		if(!string.IsNullOrEmpty(userMessage) ) 
-		{
-			messages.Add(new ChatMessage { Text = userMessage, IsIncoming = false });
-			userInputEntry.Text = string.Empty;
-		}
-	}
 }
