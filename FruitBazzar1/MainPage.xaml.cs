ed11422179b6af0428b5369d6233ac3f8a142b2d
Michelle Ling Jia Wen <lingjiawen2001424@gmail.com>
2023-11-18 00:06:43 +0800

    small upup

--- FruitBazzar1/MainPage.xaml.cs
+++ FruitBazzar1/MainPage.xaml.cs
@@ -12,7 +12,8 @@ public partial class MainPage : ContentPage
     async void TapGestureRecognizer_Tapped(System.Object sender, Microsoft.Maui.Controls.
 	TappedEventArgs e)
     {
-		await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        ////await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        await Shell.Current.GoToAsync(nameof(LoginPage));
     }
 }
 
